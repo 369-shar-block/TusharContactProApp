8a48fb587c8e0c59083e9d80ebeefba215f131e0
Tushar Saini <369shar@gmail.com>
2022-12-10 19:20:50 -0600

    Added some front end to delete and mail in contacts

--- Data/Migrations/20221211000311_0005.Designer.cs
+++ Data/Migrations/20221211000311_0005.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TusharContactProApp.Data;
@@ -11,9 +12,10 @@ using TusharContactProApp.Data;
 namespace TusharContactProApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221211000311_0005")]
+    partial class _0005
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
